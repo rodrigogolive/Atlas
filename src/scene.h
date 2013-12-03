3016b09d300acb47594a12f1c0934208bdaab237
Rodrigo Goncalves de Oliveira <rodrigo@thecoreme.org>
2013-12-03 01:00:07 -0400

    WIP Adding ImageItem and ImageItemList objects
    Chicken chicken chicken, chicken chicken.
    
    Signed-off-by: Rodrigo Goncalves de Oliveira <rodrigo@thecoreme.org>

--- src/scene.h
+++ src/scene.h
@@ -6,6 +6,42 @@
 #include <QGraphicsPixmapItem>
 #include <QList>
 
+class ImageItem
+{
+public:
+    ImageItem(QPixmap *image)
+    {
+        m_image = image;
+        m_pos = QPoint(0, 0);
+    }
+    ~ImageItem()
+    {
+        if (m_image)
+            delete m_image;
+    }
+
+    QPixmap *pixmap()
+    {
+        return m_image;
+    }
+    QSize size() const
+    {
+        if (m_image)
+            return m_image->size();
+
+        return QSize(0, 0);
+    }
+    QPoint pos() const
+    {
+        return m_pos;
+    }
+
+private:
+    QPixmap *m_image;
+    QPoint m_pos;
+};
+
+typedef QList<ImageItem *> ImageItemList;
 typedef QList<QPixmap *> QPixmapList;
 
 class Scene: public QGraphicsScene
@@ -29,6 +65,7 @@ private:
     // packing calculation; should we create a custom class to handle
     // the image list, rectangles, positions, algorithms, ...?
     QPixmapList m_pixmapList; // all images added to the scene
+    ImageItemList m_imageList; // all images added to the scene
 
     QSize m_currentSize;
 
